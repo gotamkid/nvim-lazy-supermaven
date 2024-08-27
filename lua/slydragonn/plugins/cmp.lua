0fa9caf269a9f1293a8bb435d77b7daec3886e81
gotam <131022173+gotamkid@users.noreply.github.com>
2024-08-27 12:31:15 +0530

    Update cmp.lua

--- lua/slydragonn/plugins/cmp.lua
+++ lua/slydragonn/plugins/cmp.lua
@@ -37,6 +37,7 @@ return {
 				}),
 			}),
 			sources = cmp.config.sources({
+                                { name = "supermaven" },
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "buffer" },
